a4cc2fead32c544c31ee95a8e23737883a19d7cb
miloc7 <77077563+miloc7@users.noreply.github.com>
2021-09-03 00:12:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ with open('secrets.json') as fin:
 bot = commands.Bot(command_prefix='!', help_command=None)
 @bot.command(name="whenrating")
 async def whenrating(ctx, name, rating, variant="Rapid"):
-    error_bool, error_msg, prob_success, predicted_date = await process_inputs(name,rating,variant)
+    error_bool, error_msg, prob_success, predicted_date, current_rating = await process_inputs(name,rating,variant)
     # Error that is so severe that no prediction is generated
     if error_bool and prob_success is None:
         await ctx.send(error_msg)
@@ -19,7 +19,7 @@ async def whenrating(ctx, name, rating, variant="Rapid"):
         # Warning, there's something concerning but we can still display a prediction
         if error_bool:
             msg += error_msg + '\n'
-        msg += f"{name}'s chance of reaching {rating} {variant.title()} within 2 years: **{prob_success}%**"
+        msg += f"{name}'s chance of reaching {rating} {variant.title()} within 2 years: **{prob_success}%** (currently: {current_rating})"
         if prob_success > 5:
             msg += f"\nExpected date: **{predicted_date}**."
         await ctx.send(msg)
